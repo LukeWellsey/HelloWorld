dc0d75d8a7f56365008b524df3f5fd518b504837
LukeWellsey <lukewells768@gmail.com>
2024-05-06 12:24:42 -0600

    shapes.py now calculates area

--- shapes.py
+++ shapes.py
@@ -1,8 +1,13 @@
 # python
 
 # This will calculate the shares areas.
+def calculateRectangleArea(width, height):
+    area = width * height
+    return area
 
 def main():
     print('This is our shapes program')
 
 main()
+rectangle_area = calculateRectangleArea(10,20)
+print(f'the area of the rectangle is {rectangle_area}')
